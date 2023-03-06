742fafc8223b0bc6a17136ea483bd900ab6ae469
Julian0330 <744251@student.dusd.net>
2023-03-06 11:37:02 -0800

    finished project
    finished project

--- BasicGameplayP3Ballar/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGameplayP3Ballar/Assets/Scripts/DestroyOutOfBounds.cs
@@ -10,7 +10,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 
     }
     private float topBound = 30;
-    private float lowerBound = -10; 
+    private float lowerBound = -10;
     // Update is called once per frame
     void Update()
     {
@@ -20,6 +20,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
---
+++ BasicGameplayP3Ballar/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows;
+using Input = UnityEngine.Input;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    // Start is called before the first frame update
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+
+    }
+    public float spawnRangeX = 20;
+    public float spawnPosZ = 20;
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnRandomAnimal()
+    {
+
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+
+
+    }
+}
